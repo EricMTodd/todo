7ec1722fda4cbef45fee1cdbffc094c0e410ac17
Eric M. Todd <eterictodd@gmail.com>
2021-05-07 16:05:59 -0600

    Basic user creating and login interface implemented and styled.

--- src/views/indexViews.js
+++ src/views/indexViews.js
@@ -6,26 +6,20 @@ import { storageControllers } from '../controllers/storageControllers';
 const indexViews = (() => {
 	let storageObject = storageControllers.load();
 	let main = document.querySelector('main');
-	navViews.renderLoginControlsContainer();
 
 	const renderUserIndex = () => {
+		navViews.renderLogoutButton();
 		main.innerHTML = '';
 		let container = document.createElement('div');
 		let welcomeMessage = document.createElement('h1');
 		welcomeMessage.innerText = `Welcome ${storageObject.users.loggedInUser.name}!`;
-		let link = document.createElement('a');
-		link.innerText = 'Logout';
-		link.addEventListener('click', (e) => {
-			authControllers.logout();
-		});
 		container.appendChild(welcomeMessage);
-		container.appendChild(link);
 		main.appendChild(container);
 	};
 
 	const renderNoUserIndex = () => {
+		navViews.renderLoginButtons();
 		main.innerHTML = '';
-
 		const renderAboutContainer = (() => {
 			let aboutContainer = document.createElement('div');
 			aboutContainer.className = 'container';
@@ -37,13 +31,14 @@ const indexViews = (() => {
 			aboutText.innerText = `#todo is a place to keep all your lists of things, whatver they may be. Simply create an account, login and start making your day to day more productive!`;
 			let button = document.createElement('button');
 			button.innerText = 'Get started';
+			button.disabled = true;
 			button.addEventListener('click', (e) => {
 				authViews.renderLoginForm();
 			});
 			aboutContainer.appendChild(indexHeader);
 			aboutContainer.appendChild(aboutHeader);
 			aboutContainer.appendChild(aboutText);
-			aboutContainer.appendChild(button);
+			// aboutContainer.appendChild(button);
 			main.appendChild(aboutContainer);
 		})();
 	};
--- src/views/navViews.js
+++ src/views/navViews.js
@@ -1,6 +1,7 @@
 import { indexViews } from './indexViews';
 import { usersViews } from './usersViews';
 import { authViews } from './authViews';
+import { authControllers } from '../controllers/authControllers';
 
 const navViews = (() => {
 	let nav = document.querySelector('nav');
@@ -9,7 +10,11 @@ const navViews = (() => {
 		indexViews.checkLogin();
 	});
 
-	const renderLoginControlsContainer = () => {
+	let authButtonsContainer = document.querySelector('#auth-buttons-container');
+
+	const renderLoginButtons = () => {
+		authButtonsContainer.innerHTML = '';
+
 		const renderLoginButton = (() => {
 			let button = document.createElement('button');
 			button.innerText = 'Login';
@@ -18,7 +23,7 @@ const navViews = (() => {
 			button.addEventListener('click', (e) => {
 				authViews.renderLoginForm();
 			});
-			return nav.appendChild(button);
+			return authButtonsContainer.appendChild(button);
 		})();
 		const renderCreateUserButton = (() => {
 			let button = document.createElement('button');
@@ -27,12 +32,25 @@ const navViews = (() => {
 			button.addEventListener('click', (e) => {
 				usersViews.renderCreateUserForm();
 			});
-			return nav.appendChild(button);
+			return authButtonsContainer.appendChild(button);
 		})();
 	};
 
+	const renderLogoutButton = () => {
+		authButtonsContainer.innerHTML = '';
+		let button = document.createElement('button');
+		button.innerText = 'Logout';
+		button.id = 'logout-button';
+		button.className = 'push';
+		button.addEventListener('click', (e) => {
+			authControllers.logout();
+		});
+		authButtonsContainer.appendChild(button);
+	};
+
 	return {
-		renderLoginControlsContainer,
+		renderLoginButtons,
+		renderLogoutButton,
 	};
 })();
 
